013bdcc7ef5b1d1dcfbf206e586379071c6e2c46
Nicollas César Alcântara <147162529+niklaz4@users.noreply.github.com>
2024-03-26 21:39:01 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO filmes(titulo, sinopse, anoLancamento) VALUES ('Harry Potter e a Pedra Filosofal', 'magia', 2001);
-INSERT INTO filmes(titulo, sinopse, anoLancamento) VALUES ('The Marvels', 'super heroi', 2023);
+INSERT INTO filmes(titulo, sinopse, ano_lancamento) VALUES ('Harry Potter e a Pedra Filosofal', 'magia', 2001);
+INSERT INTO filmes(titulo, sinopse, ano_lancamento) VALUES ('The Marvels', 'super heroi', 2023);
 
 INSERT INTO estudios(nome) VALUES('Warner Bros.');
 INSERT INTO estudios(nome) VALUES ('Disney');
